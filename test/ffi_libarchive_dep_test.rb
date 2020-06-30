cd04be0b796b731b13991147781127da59d6c8c2
Ben Gollmer <jatoben@github.com>
2020-06-29 22:37:34 -0700

    Bump test & README too

--- test/ffi_libarchive_dep_test.rb
+++ test/ffi_libarchive_dep_test.rb
@@ -1,11 +1,11 @@
 require "minitest/autorun"
 require "ffi_libarchive_preload"
-require "ffi_libarchive"
+require "ffi-libarchive"
 
 class FfiLibarchivePreloadTest < Minitest::Test
   def test_read_archive
     reader = Archive::Reader.open_filename("#{__dir__}/fixture.tar.gz")
-   
+
     filename = nil
     reader.each_entry do |entry|
       filename = entry.pathname
